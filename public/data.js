913753def4fff175d9264b1a8f09eef136fa9e22
Eddie Larabi <117444690+SElarabi@users.noreply.github.com>
2023-10-21 01:37:58 -0400

    standardize pages

---
+++ public/data.js
@@ -0,0 +1,28 @@
+/** @format */
+
+function data() {
+	const url = '/data';
+	const header = document.getElementById('header');
+	superagent.get(url).end(function (err, res) {
+		if (err) {
+			console.log(err);
+		} else {
+			html = '<div class="grid-container">';
+			// res.body is he data stored in the db.json
+			console.log(res.body.length);
+			if (res.body.length === 0) {
+				html += `
+								<h1 class="display-4 bg-warning text-center"> THE USER LIST IS EMPTY, ADD USERS </h1>
+								</div>
+						`;
+			} else {
+				html += `
+								<h1 class="display-4 bg-info text-center"> TOTAL USERS IN THE LIST IS = ${res.body.length}</h1>
+								</div>
+						`;
+			}
+		}
+		header.innerHTML = html;
+	});
+}
+data();
